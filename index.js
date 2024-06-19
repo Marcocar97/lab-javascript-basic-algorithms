04a42cb1644c27550b055f5e8a4a140be800949e
Marcos <mmarkito708@gmail.com>
2024-06-19 16:46:22 +0200

    primera parte

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Juan"
+
+console.log( `el nombre del conductor es ${hacker1}`)
+
+let hacker2 = "Maria"
+
+console.log(`${hacker2} Es la navegadora`)
 
 // Iteration 2: Conditionals
 
